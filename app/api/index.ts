9e711b556b8ccc128007ff21bf3661453b7ad635
Levi Robertson <levi@technomancy.dev>
2024-04-29 19:42:48 +0200

    cleanup from moving keygen

--- app/api/index.ts
+++ app/api/index.ts
@@ -13,14 +13,6 @@ import "dotenv/config";
 const app = new Hono();
 
 app.use("/*", async (c, next) => {
-  const auth_cookie = getCookie(c, "pb_auth");
-  pb.authStore.loadFromCookie(auth_cookie!);
-
-  // If they have a valid JWT just use that.
-  if (pb.authStore.isValid) {
-    return await next();
-  }
-
   return bearerAuth({
     verifyToken: async (token, c) => {
       const id = token.split("_")[0];
@@ -28,8 +20,8 @@ app.use("/*", async (c, next) => {
 
       const record = await pbadmin
         .collection("application_keys")
-        .getFirstListItem(`key_id="${id}"`, {})
-        .catch(console.log);
+        .getFirstListItem(`key_id="${id}"`)
+        .catch(console.error);
 
       c.set("aws_key", record.aws_key);
       c.set("aws_secret", record.aws_secret);
@@ -42,10 +34,6 @@ app.use("/*", async (c, next) => {
       return bcrypt.compareSync(pass, record.key_hash);
     },
   })(c, next);
-});
-
-app.get("/new-key", async (c) => {
-  return c.json(generate_key_and_hash());
 });
 
 app.route("/emails/", email);
