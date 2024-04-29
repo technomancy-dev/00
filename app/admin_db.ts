dcfd6bf1d62da0a09e0a0adc6cdc28a9ce58a3eb
Levi Robertson <levi@technomancy.dev>
2024-04-29 17:37:37 +0200

    fix comment

--- app/admin_db.ts
+++ app/admin_db.ts
@@ -5,7 +5,8 @@ import "dotenv";
 const pb = new PocketBase(
   import.meta.env.PROD ? process.env.PRODUCTION_DB_URL : "http://127.0.0.1:8090"
 );
-// // the email and password could be retrieved from env variables
+
+// the email and password could be retrieved from env variables
 await pb.admins.authWithPassword(
   process.env.ADMIN_EMAIL!,
   process.env.ADMIN_PASSWORD!,
