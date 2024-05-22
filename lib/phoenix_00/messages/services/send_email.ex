5f8f17ef50dd4d04cbb1fd349383935fbe344a4e
Levi <levi@technomancy.dev>
2024-05-22 18:18:07 +0200

    Bring back save on send email service

--- lib/phoenix_00/messages/services/send_email.ex
+++ lib/phoenix_00/messages/services/send_email.ex
@@ -10,9 +10,9 @@ defmodule Phoenix00.Messages.Services.SendEmail do
       ) do
     with {:ok, mail} <-
            MailMan.letter(to, from, subject, body, text)
-           |> Mailer.deliver() do
-      # {:ok} <-
-      # EmailRepo.create_email(merge_aws_with_email(mail, email_req)) do
+           |> Mailer.deliver(),
+         {:ok} <-
+           EmailRepo.create_email(merge_aws_with_email(mail, email_req)) do
       :ok
     else
       _ -> :error
