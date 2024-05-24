1f734afa8c370a2f7715aa8f1920bff5d33eb03b
Levi <levi@technomancy.dev>
2024-05-25 00:35:24 +0200

    markdown email support

--- lib/mail_man.ex
+++ lib/mail_man.ex
@@ -1,7 +1,9 @@
 defmodule Phoenix00.MailMan do
+  require Phoenix.Component
   import Swoosh.Email
+  import Phoenix.HTML
 
-  def letter(to, from, subject, body, text) do
+  def letter(%{to: to, from: from, subject: subject, body: body, text: text}) do
     new()
     |> to({nil, to})
     |> from({nil, from})
@@ -9,5 +11,25 @@ defmodule Phoenix00.MailMan do
     |> html_body(body)
     |> text_body(text)
     |> put_provider_option(:configuration_set_name, "default")
+  end
+
+  def letter(%{to: to, from: from, subject: subject, markdown: markdown}) do
+    html = render_markdown_to_html(markdown)
+
+    new()
+    |> to({nil, to})
+    |> from({nil, from})
+    |> subject(subject)
+    |> html_body(html)
+    |> text_body(html |> html_escape() |> safe_to_string())
+    |> put_provider_option(:configuration_set_name, "default")
+  end
+
+  # defp render_markdown(markdown) do
+  #   %{html: render_markdown_to_html(markdown)}
+  # end
+
+  defp render_markdown_to_html(markdown) do
+    MDEx.to_html(markdown)
   end
 end
--- lib/phoenix_00/messages/services/send_email.ex
+++ lib/phoenix_00/messages/services/send_email.ex
@@ -4,26 +4,29 @@ defmodule Phoenix00.Messages.Services.SendEmail do
   alias Phoenix00.Mailer
   require Logger
 
-  def call(
-        %{"from" => from, "to" => to, "subject" => subject, "body" => body, "text" => text} =
-          email_req
-      ) do
-    with {:ok, mail} <-
-           MailMan.letter(to, from, subject, body, text)
-           |> Mailer.deliver(),
-         {:ok} <-
-           EmailRepo.create_email(merge_aws_with_email(mail, email_req)) do
-      :ok
-    else
-      _ -> :error
-    end
-  end
+  def call(%{"from" => _, "to" => _, "subject" => _, "body" => _, "text" => _} = email_req),
+    do: proccess_and_send_email(email_req)
+
+  def call(%{"from" => _, "to" => _, "subject" => _, "markdown" => _} = email_req),
+    do: proccess_and_send_email(email_req)
 
   def call(req) do
     Logger.error("You are missing arguments to SendEmail called with:")
     Logger.error(req)
-    Logger.error("Expected %{to, from, subject, body, text}")
+    Logger.error("Expected %{to, from, subject, body, text} or %{to, from, subject, markdown}")
     :error
+  end
+
+  defp proccess_and_send_email(email_req) do
+    with {:ok, mail} <-
+           email_req
+           |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
+           |> MailMan.letter()
+           |> Mailer.deliver() do
+      EmailRepo.create_email(merge_aws_with_email(mail, email_req))
+    else
+      _ -> :error
+    end
   end
 
   defp merge_aws_with_email(aws, email) do
